361d51c31204399c7d24659e281a929743a24b2f
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-11-29 09:21:48 +0300

    feat: add user table

---
+++ src/main/kotlin/com/example/database/tables/User.kt
@@ -0,0 +1,9 @@
+package com.example.database.tables
+
+import org.jetbrains.exposed.sql.Table
+
+object User: Table(name = "user") {
+    val id = User.integer("id").autoIncrement()
+    val login = User.text("login")
+    val password = User.text("password")
+}
