9d6164650f333d243ce30aec7d791f909db52828
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-11-29 09:15:16 +0300

    feat: setup database

--- src/main/kotlin/com/example/database/core/DatabaseFactory.kt
+++ src/main/kotlin/com/example/database/core/DatabaseFactory.kt
@@ -1,7 +1,15 @@
 package com.example.database.core
 
 import kotlinx.coroutines.Dispatchers
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 
 object DatabaseFactory {
+    fun init() {
+        Database.connect("jdbc:postgresql://localhost:5432/custom-http-protocol", "org.postgresql.Driver",
+            "postgres", "postgres")
+    }
 
+    suspend fun <T> dbQuery(block: suspend () -> T): T =
+        newSuspendedTransaction(Dispatchers.IO) { block() }
 }
